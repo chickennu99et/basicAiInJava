398750ccd3c593404fe39442f5a71fd12342b2a3
chicken nugget <116309083+chickennu99et@users.noreply.github.com>
2026-08-28 07:39:23 -0700

    Refactor Neuron class and add new methods
    Refactored Neuron class to use learnRate and regRate. Added methods to update learning and regularization rates, and to retrieve connection weights and biases.

--- tasks/Neuron.java
+++ tasks/Neuron.java
@@ -1,23 +1,27 @@
 package tasks;
 public class Neuron
 {
-    private double biasA;
-    private double biasB;
     private double[] weightsA;
     private double[] weightsB;
+    private double biasA;
+    private double biasB;
     private final int length;
+    private double learnRate;
+    private double regRate;
     
-    public Neuron(int connectionCount, double biasInA, double biasInB) 
+    public Neuron(int connectionCount, double learnRate, double regRate) 
     {
+        learnRate = learnRate;
+        regRate = regRate;
         weightsA = new double[connectionCount];
         weightsB = new double[connectionCount];
+        biasA = Math.random()*2-1;
+        biasB = Math.random()*2-1;
         for(int i = 0; i<connectionCount; i++)
         {
             weightsA[i] = Math.random()*2-1;
             weightsB[i] = Math.random()*2-1;
         }
-        biasA = biasInA;
-        biasB = biasInB;
         length = connectionCount;
     }
     
@@ -25,11 +29,32 @@ public class Neuron
     {
         double a = biasA;
         double b = biasB;
-        
         for(int i = 0; i<length; i++){
             a+=weightsA[i]*inputs[i];
             b+=weightsB[i]*inputs[i];
         }
-        return(a*(b/(Math.exp(-b))));
+        return(a/(1.0+Math.exp(-a))*b);
+    }
+    
+    public int getConnectionCount(){
+        return(length);
+    }
+    public String getValues()
+    {
+        String returnValue = "";
+        for(int i = 0; i<length; i++)
+        {
+            returnValue+=weightsA[i] + "," + weightsB[i]+ ",";
+        }
+        returnValue += biasA + "," + biasB;
+        return(returnValue);
+    }
+    public void updateRegRate(double updatedRegRate)
+    {
+        regRate = updatedRegRate;
+    }
+    public void updateLearnRate(double updatedLearnRate)
+    {
+        learnRate = updatedLearnRate;
     }
 }
