6bb738016f9fbb06a29125b9055f002adee4f873
chicken nugget <116309083+chickennu99et@users.noreply.github.com>
2026-08-28 07:23:42 -0700

    Add layer and random input to MyProgram
    Refactored MyProgram to include a new layer and random input values.

--- MyProgram.java
+++ MyProgram.java
@@ -1,9 +1,17 @@
-import static tasks.MathStuffs.SM.*;
-
+import tasks.*;
 public class MyProgram
 {
     public static void main(String[] args)
     {
-        System.out.println(relu(3));
+        layer newLayer = new layer(16, 16);
+        double[] valuesIn = new double[4096];
+        for(int i = 0; i<valuesIn.length; i++)
+        {
+            valuesIn[i] = Math.random()*2-1;
+        }
+        valuesIn = newLayer.calculateOutputs(valuesIn);
+        for(int i = 0; i<16; i++){
+            System.out.println(valuesIn[i]);
+        }
     }
 }
