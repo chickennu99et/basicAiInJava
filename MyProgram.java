3340947a1ad8e1eadddbb2f7bdabced2d3e2f1c9
chicken nugget <116309083+chickennu99et@users.noreply.github.com>
2026-08-28 07:45:20 -0700

    Refactor MyProgram for improved logging and parameters

--- MyProgram.java
+++ MyProgram.java
@@ -11,6 +11,7 @@ public class MyProgram
 {
     public static void main(String[] args)
     {
+        String LogFileName = initLogs();
         boolean FileLoadOrNot = true;
         String init = MainChecker();
         if(init.equals(""))
@@ -18,28 +19,29 @@ public class MyProgram
             init = checkConfig();
             if(init.equals(""))
             {
-                System.err.println("exit code 2: no config found or failed to read, try deleting config.properties if it was there prior to run");
+                System.err.println("exit code 2: no config found or failed to read, try deleting config.properties if it was there prior to this execution");
                 System.exit(2);
             }else{
                 String[] valueList = (init.substring(0, init.length()-1)).split(";");
                 int epochCount = Integer.parseInt(valueList[0]);
                 double regRate = Double.parseDouble(valueList[1]);
                 double learnRate = Double.parseDouble(valueList[2]);
-                int loadType = Integer.parseInt(valueList[3]);
-                String[] arrTempOne = (valueList[4].substring(1, valueList[4].length()-1)).split(",");
+                boolean makeLog = Boolean.parseBoolean(valueList[3]);
+                int loadType = Integer.parseInt(valueList[4]);
+                String[] arrTempOne = (valueList[5].substring(1, valueList[5].length()-1)).split(",");
                 int[] array = new int[arrTempOne.length];
                 for(int i = 0; i<array.length; i++)
                 {
                     array[i] = Integer.parseInt(arrTempOne[i]);
                 }
-                int valuesIn = Integer.parseInt(valueList[5]);
-                int layerCount = Integer.parseInt(valueList[6]);
-                int ValuesIn = Integer.parseInt(valueList[7]);
-                int neuronsPerLayer = Integer.parseInt(valueList[8]);
-                int NeuronsOut = Integer.parseInt(valueList[9]);
+                int valuesIn = Integer.parseInt(valueList[6]);
+                int layerCount = Integer.parseInt(valueList[7]);
+                int ValuesIn = Integer.parseInt(valueList[8]);
+                int neuronsPerLayer = Integer.parseInt(valueList[9]);
+                int NeuronsOut = Integer.parseInt(valueList[10]);
                 if(loadType == 0)
                 {
-                    Network newNetwork = new Network(array, valuesIn, learnRate, regRate);
+                    Network newNetwork = new Network(array, valuesIn, learnRate, regRate, false);
                     double[] valuesInForCalc = {0.0};
                     for(int i = 0; i<epochCount; i++)
                     {
@@ -50,7 +52,7 @@ public class MyProgram
                 }
                 else if(loadType == 1)
                 {
-                    Network newNetwork = new Network(layerCount, ValuesIn, neuronsPerLayer, NeuronsOut, learnRate, regRate);
+                    Network newNetwork = new Network(layerCount, ValuesIn, neuronsPerLayer, NeuronsOut, learnRate, regRate, true);
                     double[] valuesInForCalc = {0.0};
                     for(int i = 0; i<epochCount; i++)
                     {
@@ -66,13 +68,13 @@ public class MyProgram
                 }
             }
         }else{
-            System.out.println(isValidFile(init));
-            
-            //System.out.println(isValidFile(init));
-            
+            if(!isValidFile(init)){
+                System.err.println("Exit code 3:\nSave: " + init + "; appears to be corrupted, try deleting the file or undoing changes you did, it will default to the next availible save or return to config on deletion of the save");
+                System.exit(3);
+            }
             
             System.err.println("not implemented file load yet");
-            System.exit(3);
+            System.exit(4);
         }
     }
     
