c4e988cc3a645db85462dcea182a0a7a07482efe
chicken nugget <116309083+chickennu99et@users.noreply.github.com>
2026-08-28 08:00:22 -0700

    Update saveReader.java
    apart of v4

--- NonNetwork/saveReader.java
+++ NonNetwork/saveReader.java
@@ -6,8 +6,6 @@ import java.io.IOException;
 
 import static java.lang.Integer.parseInt;
 
-
-
 public class saveReader 
 {
     public static boolean isValidFile(String filename)
@@ -18,20 +16,54 @@ public class saveReader
         {
             intArrayCheck[i] = Integer.parseInt(lineOne[i]);
         }
-        
         int lineCountForFutureRef = getLineCount(filename);
-        
         if(!isValidFileCheckOne(filename, lineCountForFutureRef, intArrayCheck))
         {
-            System.err.println("Exit code 3:\nSave: " + filename + "; appears to be corrupted, try deleting the file or undoing changes you did, it will default to the next availible save or return to config on deletion of the save");
-            
-            System.exit(3);
+            return(false);
+        }
+        
+        if(!validateAllNeurons(filename, intArrayCheck, lineCountForFutureRef))
+        {
+            return(false);
         }
         
         return(true);
         
     }
-    public static boolean isValidFileCheckOne(String filename, int LC, int[] intArrayCheck){
+    
+    public static boolean validateAllNeurons(String filename, int[] L1, int LC)
+    {
+        int linesToCheckStart = 1;
+        for(int i = 1; i<L1.length; i++)
+        {
+            System.out.println("reading lines: " + (linesToCheckStart + 1) + " -> " + (linesToCheckStart + L1[i]));
+            if(!validateLines(filename, linesToCheckStart, L1[i], L1[i-1]))
+            {
+                return(false);
+            }
+            System.out.println("");
+            linesToCheckStart+=L1[i];
+        }
+        return(true);
+    }
+    
+    public static boolean validateLines(String filename, int startLine, int linesAfter, int priorLines)
+    {
+        for(int i = startLine; i<startLine+linesAfter; i++)
+        {
+            System.out.println("Checking variable count of line and comparing it to the excpected count...");
+            if( (getCharCount(readLine(filename,i), ',')-1) != priorLines*2)
+            {
+                System.out.println("incorrect line count");
+                return(false);
+            }
+            System.out.println("Correct line count");
+            //System.out.println(getCharCount(readLine(filename,i), ',')-1);
+        }
+        return(true);
+    }
+    public static boolean isValidFileCheckOne(String filename, int LC, int[] intArrayCheck)
+    {
         int lineCountExpected = 1;
         for(int i = 1; i<intArrayCheck.length; i++)
         {
@@ -41,7 +73,7 @@ public class saveReader
     }
     public static String readLine(String filename, int line)
     {
-        System.out.println("reading line " + line + " of file: *" + filename + "...");
+        System.out.println("reading line " + (line + 1) + " of file: *" + filename + "...");
         try(BufferedReader br = new BufferedReader(new FileReader(filename)))
         {
             String str;
@@ -77,5 +109,16 @@ public class saveReader
             System.out.println(e);
         }
         return(index);
+    }
+    public static int getCharCount(String str, char x)
+    {
+        int i = 0;
+        int c = 0;
+        while((i = str.indexOf(x, i)) != -1)
+        {
+            i++;
+            c++;
+        }
+        return(c);
     }
 }
